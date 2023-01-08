139316666de27540589a66f49c2d55620b597667
Fatih Aydın <fatihaydin@temizlikyolda.com>
2023-01-08 10:00:27 +0300

    Removed `method`, `positionalArguments` and `namedArguments` params from `pipeThrough` method in `PipelineMixin`

--- lib/src/pipeline_mixin.dart
+++ lib/src/pipeline_mixin.dart
@@ -3,9 +3,6 @@ import 'package:pipeline_plus/pipeline_plus.dart';
 mixin PipelineMixin {
   Pipeline pipeThrough({
     required List pipes,
-    String method = 'handle',
-    List<dynamic> positionalArguments = const [],
-    Map<Symbol, dynamic> namedArguments = const <Symbol, dynamic>{},
   }) {
     return Pipeline()
       ..send(data: this)
